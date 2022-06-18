029ce6cf176f62a55c956c9ae1249eb85ec2281f
leizhang <lzphy@umich.edu>
2022-06-18 17:07:08 -0400

    add single-flip

--- src/Ising_2d.cpp
+++ src/Ising_2d.cpp
@@ -1,9 +1,38 @@
 #include "Ising_2d.hpp"
 
-Lattice::Lattice(int L, double beta) : L_(L), beta_(beta), config_(L_, std::vector<int > (L, 1)) {
+Lattice::Lattice(int L, double beta) : L_(L), beta_(beta), config_(L_, std::vector<int > (L, 1)), E_(-2.0 * L * L), M_(1.0 * L * L) {
 }
 
 int Lattice::L(){
   return L_;
 }
 
+void Lattice::update_E(){
+ double sum=0.0;
+ for (int i=0; i<L_; i++){
+   for(int j=0;j<L_;j++){
+       sum+=-1.0*config_[i][j]*config_[i][(j+1)%L_];
+       sum+=-1.0*config_[i][j]*config_[(i+1)%L_][j];
+   }
+ }
+ E_ = sum;
+};
+
+
+void Lattice::update_M(){
+  double sum=0.0;
+  for (int i=0; i<L_; i++){
+    for(int j=0;j<L_;j++){
+      sum+=1.0*config_[i][j];
+    }
+  }
+  M_ = sum;
+}
+
+
+
+void Lattice::flip( int i, int j){
+  config_[i][j] = - config_[i][j];
+  update_E();
+  update_M();
+}
--- test/Test.cpp
+++ test/Test.cpp
@@ -3,13 +3,24 @@
 
 TEST(Ising, ConfigSetup){
   Lattice Ising(10, 1.0);
- // Ising.setup_config();
   int L = Ising.L();
   for(int i = 0; i < L; i++) {
     for(int j = 0; j < L; j++) {
       EXPECT_EQ(Ising.config()[i][j], 1);
     }
   }
-  
 }
 
+TEST(Ising, Observable){
+  int L=10;
+  double tol = 1.e-10;
+  Lattice Ising(L,1.0);
+  double E=Ising.get_E();
+  EXPECT_NEAR(E, -2.0*L*L, tol);
+  double M=Ising.get_M();
+  EXPECT_NEAR(M, 1.0*L*L, tol);
+  
+  Ising.flip(0,0);
+  EXPECT_NEAR(Ising.get_E(), -2 * L * L + 8.0, tol);
+  EXPECT_NEAR(Ising.get_M(), 1.0 * L * L - 2.0, tol);  
+}
